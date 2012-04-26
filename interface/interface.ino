924461a608cd2251a09d1fe7c270fb4745ab7a81
David Puzey <david.puzey@gmail.com>
2012-04-26 12:53:06 +0100

    Seemingly reads serial properly

--- interface/interface.ino
+++ interface/interface.ino
@@ -1,14 +1,29 @@
+String inString;
+
 void setup() {
-  
+    Serial.begin(9600);
 }
 
-void loop() {
-  
+void loop()
+{
+    while (Serial.available() > 0) {
+      delay(5);
+      
+      inString += (char) Serial.read();
+    }
+    if (inString.length() > 0) {
+      Serial.println(inString);
+      inString = "";
+    }
 }
 
-String getSerialString() {
-  int sAvail = Serial.available();
-  if (sAvail <= 0)
-    return "";
+/**
+ * chkCommand - Checks for a new command on the serial interface
+ */
+void chkCommand() {
+  if (Serial.available() <= 0)
+    return;
+  char cmdChar = Serial.read();
   
+  Serial.flush(); // Clear out the buffer
 }

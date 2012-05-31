6298b8450fc0f39ecc336256fac78a4f0780a0dd
David Puzey <david.puzey@gmail.com>
2012-05-31 12:08:43 +0100

    Much better Arduino code

--- interface/interface.ino
+++ interface/interface.ino
@@ -6,9 +6,9 @@ const int ERROR_LT_CHARS = 2; // Not enough parameters for function
 const int ERROR_WHEEL_OUT_OF_RANGE = 3; // The wheel selected doesn't exist
 
 /* Set pin assignments */
-const int IRPD_SENSOR_LEFT = 0; // The pin for polling the left side of the IRPD
-const int IRPD_SENSOR_RIGHT = 1; // The pin for polling the right side of the IRPD
-const int IRPD_RESPONSE = 2; // The pin for reading the response from the IRPD
+const int IRPD_SENSOR_LEFT = 12; // The pin for polling the left side of the IRPD
+const int IRPD_SENSOR_RIGHT = 13; // The pin for polling the right side of the IRPD
+const int IRPD_RESPONSE = 4; // The pin for reading the response from the IRPD
 const int SERVO_WHEEL_FRONT_LEFT = 3; // The pin for the servo controlling the front left wheel
 const int SERVO_WHEEL_FRONT_RIGHT = 5; // The pin for the servo controlling the front right wheel
 const int SERVO_WHEEL_BACK_LEFT = 6; // The pin for the servo controlling the back left wheel
@@ -18,9 +18,10 @@ const int ULTRASONIC_ECHO = 8; // The pin for receiving the echo from the ultras
 const int SERVO_PLATFORM_X = 10; // The pin for the servo controlling the platforms X rotation
 const int SERVO_PLATFORM_Y = 11; // The pin for the servo controlling the platforms Y rotation
 
-const int MAX_SPEED = 20; // The maximum speed that the servos can go
+//const int MAX_SPEED = 20; // The maximum speed that the servos can go
 
 Servo wheels[4], platform[2];
+int wheelOffset[4] = {0,0,0,0};
 
 void setup() {
     Serial.begin(9600);
@@ -29,10 +30,12 @@ void setup() {
     pinMode(IRPD_SENSOR_LEFT, OUTPUT);
     pinMode(IRPD_SENSOR_RIGHT, OUTPUT);
     pinMode(IRPD_RESPONSE, INPUT);
-    wheels[0].attach(SERVO_WHEEL_FRONT_LEFT);
-    wheels[1].attach(SERVO_WHEEL_FRONT_RIGHT);
-    wheels[2].attach(SERVO_WHEEL_BACK_LEFT);
-    wheels[3].attach(SERVO_WHEEL_BACK_RIGHT);
+    // We limit the minimum and maximum pulse as the servos will not go any faster for larger or smaller values
+    // We also apply an offset to each of the wheels to easier to calibrate them
+    wheels[0].attach(SERVO_WHEEL_FRONT_LEFT, (1300 + wheelOffset[0]), (1700 + wheelOffset[0]));
+    wheels[1].attach(SERVO_WHEEL_FRONT_RIGHT, (1300 + wheelOffset[1]), (1700 + wheelOffset[1]));
+    wheels[2].attach(SERVO_WHEEL_BACK_LEFT, (1300 + wheelOffset[2]), (1700 + wheelOffset[2]));
+    wheels[3].attach(SERVO_WHEEL_BACK_RIGHT, (1300 + wheelOffset[3]), (1700 + wheelOffset[3]));
     platform[0].attach(SERVO_PLATFORM_X);
     platform[1].attach(SERVO_PLATFORM_Y);
     changeAllMotors(0,0,0,0);
@@ -72,16 +75,25 @@ void movePlatform(int *params) {
  * readUltrasonic - Sends the ultrasonic reading to the computer
  * Params:
  *   params (*int) Anything, it doens't matter, it's only here so the serial reading function works properly
+ * NOTE: How to find distance from ultrasonic:
+ *       The value returned is in microseconds and we want to get the distance in centimetres
+ *       The speed of sound is about 343 metres per second
+ *       To get centimeters per microsecond we need to times by 100 & divide by 1000000 (can just divide by 10000)
+ *       This gives us 0.0343. As the value returned is for the distance there and back we need to divide by 2
+ *       Now we have 0.01715 that we can times our value by, as this isn't a nice number to work with we can get
+ *       the recipricol of this number and then divide our value instead this gives us 58.209... we can round this to 58
  */
 void readUltrasonic(int* params) {
   unsigned long int echo;
+  int distance;
   digitalWrite(ULTRASONIC_PULSE, HIGH); // Send high pulse to trigger ultrasonic detector
   delayMicroseconds(20); // Delay so that there is a long enough pulse to trigger the ultrasonic detector. Technically 10us is fine, but 20 to be safe.
   digitalWrite(ULTRASONIC_PULSE, LOW); // Stop pulse to ultrasonic
   echo = pulseIn(ULTRASONIC_ECHO, HIGH, 40000); // Read in the response, shouldn't exceed 36000, so 40000 max wait will be perfectly fine
+  distance = echo / 58; // Get the distance in cm
   delay(10); // Ensure there will be at least 10ms before the next pulse
   Serial.print("Ultrasonic reading: ");
-  Serial.println(echo);
+  Serial.println(distance);
 }
 
 /**
@@ -201,6 +213,7 @@ void chkCommand() {
     if (maxChars < minChars)
       maxChars = minChars;
     
+    Serial.println(Serial.available());
     int *paramChars = getMoreChars(minChars, maxChars);
     if (minChars > 0 && paramChars == '\0')
       error(ERROR_LT_CHARS); // Not enough characters have been provided for the function
@@ -275,17 +288,17 @@ void changeAllMotors(int fl_motor, int fr_motor, int bl_motor, int br_motor) {
  * changeMotorSpeed - change the speed of a selected motor
  * Params:
  *   motor (int) Which motor to change the speed of, range 0 to 3
- *   spd (int) The speed to change the motor too, range +- MAX_SPEED
+ *   spd (int) The speed to change the motor too
  */
 void changeMotorSpeed(int motor, int spd) {
   if (motor < 0 || motor > 3) { // Ensure the motor exists
     error(ERROR_WHEEL_OUT_OF_RANGE);
     return;
   }
-  if (spd < -MAX_SPEED) // Ensure the speed is within range
+  /*if (spd < -MAX_SPEED) // Ensure the speed is within range
     spd = -MAX_SPEED;
   else if (spd > MAX_SPEED)
-    spd = MAX_SPEED;
+    spd = MAX_SPEED;*/
   if (motor == 0 || motor == 2) // Correct direction for left side as the servos are mounted the opposite way round
     spd *= -1;
   spd = spd + 90; // Turn the speed into a servo setting

812cf6e8906dbe3502d628c79a606e28c052aaa2
David Puzey <david.puzey@gmail.com>
2012-06-01 13:19:29 +0100

    Removed the old ball detect and moved test.cpp to ball_detect.cpp ... should really have done this with git move, but meh

--- vision/ball_detect.cpp
+++ vision/ball_detect.cpp
@@ -1,85 +1,63 @@
- #include "opencv2/objdetect/objdetect.hpp"
- #include "opencv2/highgui/highgui.hpp"
- #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
- #include <iostream>
- #include <stdio.h>
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
 
- using namespace std;
- using namespace cv;
+using namespace std;
+using namespace cv;
 
- /** Function Headers */
- void detectAndDisplay( Mat frame );
+const double PI = 3.141592;
 
- /** Global variables */
- String face_cascade_name = "haarcascade_frontalface_alt.xml";
- String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
- CascadeClassifier face_cascade;
- CascadeClassifier eyes_cascade;
- string window_name = "Capture - Face detection";
- RNG rng(12345);
+int main(int argc, const char** argv) {
+// CvFont font;
+//cvinitFont(&font, CV_FONT_HERSHEY_SIMPLEX, 1.0,1.0,0,1,CV_AA);
+//cvPutText(im, "text here", cvPoint(10,130), &font, cvScalar(255,255,255,0));
+//putText(frame, area, cvPoint(0,0), FONT_HERSHEY_SIMPLEX, 10, Scalar(255,255,255));
 
- /** @function main */
- int main( int argc, const char** argv )
- {
-   CvCapture* capture;
-   Mat frame;
+	Mat frame, hsvFrame, thresholdFrame; // Frames
+	VideoCapture capture(1); // Open camera 0
 
-   //-- 1. Load the cascades
-   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-   if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+	if (!capture.isOpened()) {
+		printf("Could not capture from camera.");
+		return 1;
+	}
+	
+	while (true) {
+		capture >> frame;
+		
+		if (frame.empty()) {
+			printf("Could not get frame.");
+			return 2;
+		}
+		
+		//GaussianBlur(frame, frame, Size(5,5), 1.2, 1.2);
+		//erode(frame, frame, Mat());
+		cvtColor(frame, hsvFrame, CV_BGR2HSV);
+		inRange(hsvFrame, Scalar(50, 30, 30), Scalar(90, 255, 255), thresholdFrame);
+		
+		Moments moment = moments(thresholdFrame, true);
+		double moment10 = moment.m10;
+		double moment01 = moment.m01;
+		double area = moment.m00;
+		
+		int posx = moment10 / area;
+		int posy = moment01 / area;
+		// Radius - A=pi*r^2 --- r=sqrt(A/pi)
+		int radius = sqrt(area/PI);
 
-   //-- 2. Read the video stream
-   capture = cvCaptureFromCAM( -1 );
-   if( capture )
-   {
-     while( true )
-     {
-   frame = cvQueryFrame( capture );
-
-   //-- 3. Apply the classifier to the frame
-       if( !frame.empty() )
-       { detectAndDisplay( frame ); }
-       else
-       { printf(" --(!) No captured frame -- Break!"); break; }
-
-       int c = waitKey(10);
-       if( (char)c == 'c' ) { break; }
-      }
-   }
-   return 0;
- }
-
-/** @function detectAndDisplay */
-void detectAndDisplay( Mat frame )
-{
-  std::vector<Rect> faces;
-  Mat frame_gray;
-
-  cvtColor( frame, frame_gray, CV_BGR2GRAY );
-  equalizeHist( frame_gray, frame_gray );
-
-  //-- Detect faces
-  face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-
-  for( int i = 0; i < faces.size(); i++ )
-  {
-    Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-    ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-
-    Mat faceROI = frame_gray( faces[i] );
-    std::vector<Rect> eyes;
-
-    //-- In each face, detect eyes
-    eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-
-    for( int j = 0; j < eyes.size(); j++ )
-     {
-       Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
-       int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-       circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-     }
-  }
-  //-- Show what you got
-  imshow( window_name, frame );
- }
+		if (area > 50)
+			circle(frame, Point(posx,posy), radius, Scalar(100,50,0), 4, 8, 0);
+		
+		//putText(frame, area, cvPoint(10,10), FONT_HERSHEY_SIMPLEX, 10, Scalar(255,255,255));
+		imshow("Detected Ball", frame);
+		//imshow("tmp frame", tmpFrame);
+		
+		int exit = waitKey(10);
+		if((char)exit == 'q')
+			break;
+	}
+	return 0;
+}

b2dd38b98f341bfd0be837577ebbb574a9753e10
David Puzey <david.puzey@gmail.com>
2012-06-25 17:03:00 +0100

    Added test.cpp to the build script to make like easier when building the test script

--- overall/build.sh
+++ overall/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 g++ -ggdb `pkg-config --cflags opencv` -o overall overall.cpp rs232.c `pkg-config --libs opencv`
+g++ -ggdb `pkg-config --cflags opencv` -o test test.cpp rs232.c `pkg-config --libs opencv`

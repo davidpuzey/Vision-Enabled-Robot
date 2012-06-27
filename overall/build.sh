1c3509ef8518f18132ee82732ec927fe1353475b
David Puzey <david.puzey@gmail.com>
2012-06-27 16:03:47 +0100

    Stopped test from building since this isn't always relevant

--- overall/build.sh
+++ overall/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "Building overall"
 g++ -ggdb `pkg-config --cflags opencv` -o overall overall.cpp rs232.c `pkg-config --libs opencv`
-g++ -ggdb `pkg-config --cflags opencv` -o test test.cpp rs232.c `pkg-config --libs opencv`
+#echo "Building test"
+#g++ -ggdb `pkg-config --cflags opencv` -o test test.cpp rs232.c `pkg-config --libs opencv`

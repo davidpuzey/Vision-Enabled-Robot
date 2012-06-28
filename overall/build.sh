2cb18ece8607bf3cedf5550ddfe64668b96119a9
David Puzey <david.puzey@gmail.com>
2012-06-28 13:07:13 +0100

    Removed irrelevant build script

--- overall/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Building overall"
-g++ -ggdb `pkg-config --cflags opencv` -o overall overall.cpp rs232.c `pkg-config --libs opencv`
-#echo "Building test"
-#g++ -ggdb `pkg-config --cflags opencv` -o test test.cpp rs232.c `pkg-config --libs opencv`

40e3bed346acf4ebf0cfc9c0b77ea3a38a444d66
David Puzey <david.puzey@gmail.com>
2012-06-29 13:33:40 +0100

    Minor insignificant changes that I'm commiting so I don't forget

--- overall/serialReceive.cpp
+++ overall/serialReceive.cpp
@@ -5,12 +5,12 @@
 #include <cmath>
 
 using namespace std;
+unsigned char buf[4096];
 
 int main(int argc, const char** argv) {
 	int cport_nr=0,        /* 0 is Arduino com 0 and 1 is Arduino com 1 */
 		bdrate=9600,       /* 9600 baud */
 		n, i;
-	unsigned char buf[4096];
 	
 	if(OpenComport(cport_nr, bdrate)) {
 		printf("Can not open comport\n");
@@ -22,7 +22,7 @@ int main(int argc, const char** argv) {
 		if (n > 0) {
 			for (i=0; i < n; i++) {
 				if (buf[i] < 32)
-					printf(";%i;", buf[i]);
+					printf("%i;", buf[i]);
 			}
 			buf[n] = '\0';
 			printf("Received %i bytes: %s\n", n, (char *)buf);

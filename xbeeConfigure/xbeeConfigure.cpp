f4211e5f593e8a99affdb1b3535c00f84fbafa70
David Puzey <david.puzey@gmail.com>
2012-07-13 14:33:45 +0100

    Sends commands

--- xbeeConfigure/xbeeConfigure.cpp
+++ xbeeConfigure/xbeeConfigure.cpp
@@ -12,7 +12,8 @@ unsigned char buf[4096];
 int cport_nr=0;
 
 int main(int argc, const char** argv) {
-	int bdrate=9600;       /* baud rate */
+	int bdrate=9600;
+	string configCmd;
 	pthread_t thread_id;
 	
 	if(OpenComport(cport_nr, bdrate)) {
@@ -24,17 +25,22 @@ int main(int argc, const char** argv) {
 		printf("Camera thread could not be created.\n");
 		exit(1);
 	}
+	
+	SendBuf(cport_nr, (unsigned char*)"+++", 3);
+	
+	char cr = 13;
+	while (1) {
+		getline(cin, configCmd);
+		configCmd.push_back(cr); // Add a carriage return as it seems the xbee module doesn't like line feeds
+		SendBuf(cport_nr, (unsigned char*)configCmd.c_str(), configCmd.size()); // Send the command
+	}
 }
 
 void *ReadSerial(void *param) {
-	int i, n;
+	int n;
 	while (1) {
 		n = PollComport(cport_nr, buf, 4096);
 		if (n > 0) {
-			for (i=0; i < n; i++) {
-				if (buf[i] < 32)
-					printf("%i;", buf[i]);
-			}
 			buf[n] = '\0';
 			printf("Received: %s\n", (char *)buf);
 		}
